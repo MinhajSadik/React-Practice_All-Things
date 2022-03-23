ffd43c66dac43155bdfcc8609487a91a2fc26116
MinhajSadik <MinhajoSadik@gmail.com>
2022-03-23 19:33:16 +0600

    [ADD/START] working in new workspace

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import User from "./Components/User";
+
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <User render={(isLoggedIn) => (isLoggedIn ? "Minaj" : "Sharmina")} />
     </div>
   );
 };
