8f10ed8983728c50e5804a997e8e6f2a513df78b
MinhajSadik <MinhajoSadik@gmail.com>
2022-03-24 19:26:52 +0600

    [ADD] nothing

--- src/Components/BoilingVerdict.js
+++ src/Components/BoilingVerdict.js
@@ -1,8 +1,8 @@
 function BoilingVerdict({ celsius = 0 }) {
   if (celsius >= 100) {
-    return <p>The water would boil.</p>;
+    return <p>The water would boil</p>;
   }
-  return <p>The water would not boil.</p>;
+  return <p>The water would not boil</p>;
 }
 
 export default BoilingVerdict;
