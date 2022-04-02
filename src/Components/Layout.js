bc7c15a67a8d6483349c06643fa6c0e0a610efe5
MinhajSadik <MinhajoSadik@gmail.com>
2022-04-02 23:28:08 +0600

    [ADD] trying to make an arrow function

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,10 +1,12 @@
 import useWindowWidth from "./Hooks/useWindowWidth";
 
-export default function Layout() {
+const Layout = () => {
   const onSmallScreen = useWindowWidth(600);
   return (
     <div>
       <h1>You're Using {onSmallScreen ? "Small" : "Large"} Screen</h1>
     </div>
   );
-}
+};
+
+export default Layout;
